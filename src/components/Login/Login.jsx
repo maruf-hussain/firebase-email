a491bbe59d11a494d9938eaca9cd9ee9832568ed
maruf-hussain <engmarufhussain@gmail.com>
2023-08-24 19:28:53 +0600

    fajlf

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,17 @@
-import {  getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
+import {  getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
 import app from '../../Firebase/firebase-config';
 import { Link } from 'react-router-dom';
 
+
+
+const auth = getAuth(app);
+
 const Login = () => {
 
-    
  const [error , setError] = useState('');
  const [succes, setSucces] = useState('');  
-
-
- const auth = getAuth(app);
-
+ const emailRef = useRef();
 
 const handleSubmit = (event) => {
   event.preventDefault();
@@ -37,15 +37,34 @@ const handleSubmit = (event) => {
 
 }
 
+const resetPassword = (event) =>{
+  const email = emailRef.current.value;
+  if(!email){
+    alert('write youre proper email address')
+  }
+
+  sendPasswordResetEmail(auth, email)
+  .then(() => {
+    alert('please check your email then reset password')
+  })
+  .catch((error) => {
+   
+    const errorMessage = error.message;
+    alert(errorMessage);
+   
+  });
+}
+
     return (
         <div>
           <form className='mx-auto w-50' onSubmit ={handleSubmit}>
-                <input type="email" name="email" id="email" placeholder='Type Your Email' required /><br/>
+                <input type="email" name="email" id="email" ref={emailRef}  placeholder='Type Your Email' required /><br/>
                 <input  type="password" name="password" id="password" placeholder='Type Your Password' required />
                <br />
                <p className='text-danger'>{error}</p>
                <input style={{backgroundColor:'greenyellow'}} type="submit" value="
                Submit" />
+                <button onClick={resetPassword}>Forgate Password</button>
                <p>You Are New Please Register <Link to="/register">Register</Link></p>
                <p className='text-success'>{succes}</p>
             </form>
--- src/components/Resgister/Register.jsx
+++ src/components/Resgister/Register.jsx
@@ -10,7 +10,7 @@ const auth = getAuth(app);
 const Register = () => {
  const [error , setError] = useState('');
  const [succes, setSucces] = useState('');  
-const emailRef = useRef();
+
 
 
 
@@ -47,34 +47,19 @@ const sendVarificationEmail = (user) =>{
   })
 }
 
-const resetPassword = (event) =>{
-  const email = emailRef.current.value;
-  if(!email){
-    alert('write youre proper email address')
-  }
-  sendPasswordResetEmail(auth, email)
-  .then(() => {
-    alert('please check your email then reset password')
-  })
-  .catch((error) => {
-   
-    const errorMessage = error.message;
-    alert(errorMessage);
-   
-  });
 
-}
+
 
 
     return (
         <div>
             <form className='mx-auto w-50' onSubmit ={handleSubmit}>
-                <input type="email" name="email" id="email" ref={emailRef} placeholder='Type Your Email' required /><br/>
+                <input type="email" name="email" id="email" placeholder='Type Your Email' required /><br/>
                 <input  type="password" name="password" id="password" placeholder='Type Your Password' required />
                <br />
                <p className='text-danger'>{error}</p>
                <input style={{backgroundColor:'greenyellow'}} type="submit" value="Register" /><br/>
-                <button onClick={resetPassword}>Forgate Password</button>
+               
                <p>You Are Already Register Please Login <Link to="/login">Login</Link></p>
 
                <p className='text-success'>{succes}</p>
