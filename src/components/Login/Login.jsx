e799a31a040ef27cc048c507aa7381b5dd0d9f28
maruf-hussain <engmarufhussain@gmail.com>
2023-08-24 16:16:33 +0600

    email password final

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import {  getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import app from '../../Firebase/firebase-config';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    
+ const [error , setError] = useState('');
+ const [succes, setSucces] = useState('');  
+
+
+ const auth = getAuth(app);
+
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  setSucces('');
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  console.log(email,password);
+
+  signInWithEmailAndPassword(auth,email,password)
+  .then(result => {
+   const loggedUser = result.user;
+   console.log(loggedUser);
+   if(!loggedUser.emailVerified)
+   setSucces('User Loggin succesfully');
+   setError('');
+   event.target.reset();
+  })
+  .catch(error => {
+    console.log('errror', error.message)
+    setError('Loggin failed please appropiate keyword')
+    
+  })
+
+}
+
     return (
         <div>
-            Login
+          <form className='mx-auto w-50' onSubmit ={handleSubmit}>
+                <input type="email" name="email" id="email" placeholder='Type Your Email' required /><br/>
+                <input  type="password" name="password" id="password" placeholder='Type Your Password' required />
+               <br />
+               <p className='text-danger'>{error}</p>
+               <input style={{backgroundColor:'greenyellow'}} type="submit" value="
+               Submit" />
+               <p>You Are New Please Register <Link to="/register">Register</Link></p>
+               <p className='text-success'>{succes}</p>
+            </form>
         </div>
     );
 };
--- src/components/Resgister/Register.jsx
+++ src/components/Resgister/Register.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { useRef, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
 import app from '../../Firebase/firebase-config';
+import { Link } from 'react-router-dom';
 
 
 
@@ -9,7 +10,7 @@ const auth = getAuth(app);
 const Register = () => {
  const [error , setError] = useState('');
  const [succes, setSucces] = useState('');  
-
+const emailRef = useRef();
 
 
 
@@ -27,25 +28,55 @@ const handleSubmit = (event) => {
    setSucces('User has create succesfully');
    setError('');
    event.target.reset();
+   sendVarificationEmail(result.user);
+
   })
   .catch(error => {
     console.log('errror', error.message)
     setError('Please write keyword')
     
+    
   })
 
 }
+const sendVarificationEmail = (user) =>{
+  sendEmailVerification(user)
+  .then(result => {
+    console.log(result)
+    alert('Please varify your email check your mail inbox or spam folder')
+  })
+}
 
+const resetPassword = (event) =>{
+  const email = emailRef.current.value;
+  if(!email){
+    alert('write youre proper email address')
+  }
+  sendPasswordResetEmail(auth, email)
+  .then(() => {
+    alert('please check your email then reset password')
+  })
+  .catch((error) => {
+   
+    const errorMessage = error.message;
+    alert(errorMessage);
+   
+  });
+
+}
 
 
     return (
         <div>
             <form className='mx-auto w-50' onSubmit ={handleSubmit}>
-                <input type="email" name="email" id="email" placeholder='Type Your Email' required /><br/>
+                <input type="email" name="email" id="email" ref={emailRef} placeholder='Type Your Email' required /><br/>
                 <input  type="password" name="password" id="password" placeholder='Type Your Password' required />
                <br />
                <p className='text-danger'>{error}</p>
-               <input style={{backgroundColor:'greenyellow'}} type="submit" value="Register" />
+               <input style={{backgroundColor:'greenyellow'}} type="submit" value="Register" /><br/>
+                <button onClick={resetPassword}>Forgate Password</button>
+               <p>You Are Already Register Please Login <Link to="/login">Login</Link></p>
+
                <p className='text-success'>{succes}</p>
             </form>
            
